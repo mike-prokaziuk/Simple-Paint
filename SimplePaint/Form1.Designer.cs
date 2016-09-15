f865de3f565a9049c96f8b445f26986089d79542
Mikhail Prokazyuk <mike.prokazyuk@gmail.com>
2016-09-15 20:35:34 +0300

    built UI

--- SimplePaint/Form1.Designer.cs
+++ SimplePaint/Form1.Designer.cs
@@ -28,20 +28,170 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.menuStrip = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStrip = new System.Windows.Forms.ToolStrip();
+            this.pencilBtn = new System.Windows.Forms.ToolStripButton();
+            this.lineBtn = new System.Windows.Forms.ToolStripButton();
+            this.rectangleBtn = new System.Windows.Forms.ToolStripButton();
+            this.circleBtn = new System.Windows.Forms.ToolStripButton();
+            this.statusStrip = new System.Windows.Forms.StatusStrip();
+            this.toolStripProgressBar1 = new System.Windows.Forms.ToolStripProgressBar();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.changeMainColorBtn = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.changeFillColorBtn = new System.Windows.Forms.ToolStripButton();
+            this.menuStrip.SuspendLayout();
+            this.toolStrip.SuspendLayout();
+            this.statusStrip.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // menuStrip
+            // 
+            this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip.Name = "menuStrip";
+            this.menuStrip.Size = new System.Drawing.Size(624, 24);
+            this.menuStrip.TabIndex = 0;
+            this.menuStrip.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // toolStrip
+            // 
+            this.toolStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.pencilBtn,
+            this.lineBtn,
+            this.rectangleBtn,
+            this.circleBtn,
+            this.toolStripSeparator1,
+            this.changeMainColorBtn,
+            this.toolStripSeparator2,
+            this.changeFillColorBtn});
+            this.toolStrip.Location = new System.Drawing.Point(0, 24);
+            this.toolStrip.Name = "toolStrip";
+            this.toolStrip.Size = new System.Drawing.Size(624, 25);
+            this.toolStrip.TabIndex = 1;
+            this.toolStrip.Text = "toolStrip1";
+            // 
+            // pencilBtn
+            // 
+            this.pencilBtn.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.pencilBtn.Image = global::SimplePaint.Properties.Resources.Pencil;
+            this.pencilBtn.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.pencilBtn.Name = "pencilBtn";
+            this.pencilBtn.Size = new System.Drawing.Size(23, 22);
+            // 
+            // lineBtn
+            // 
+            this.lineBtn.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.lineBtn.Image = global::SimplePaint.Properties.Resources.Line;
+            this.lineBtn.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.lineBtn.Name = "lineBtn";
+            this.lineBtn.Size = new System.Drawing.Size(23, 22);
+            this.lineBtn.Text = "toolStripButton2";
+            // 
+            // rectangleBtn
+            // 
+            this.rectangleBtn.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.rectangleBtn.Image = global::SimplePaint.Properties.Resources.Rectange;
+            this.rectangleBtn.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.rectangleBtn.Name = "rectangleBtn";
+            this.rectangleBtn.Size = new System.Drawing.Size(23, 22);
+            this.rectangleBtn.Text = "toolStripButton3";
+            // 
+            // circleBtn
+            // 
+            this.circleBtn.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.circleBtn.Image = global::SimplePaint.Properties.Resources.Circle;
+            this.circleBtn.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.circleBtn.Name = "circleBtn";
+            this.circleBtn.Size = new System.Drawing.Size(23, 22);
+            this.circleBtn.Text = "toolStripButton4";
+            // 
+            // statusStrip
+            // 
+            this.statusStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripProgressBar1});
+            this.statusStrip.Location = new System.Drawing.Point(0, 419);
+            this.statusStrip.Name = "statusStrip";
+            this.statusStrip.Size = new System.Drawing.Size(624, 22);
+            this.statusStrip.TabIndex = 2;
+            this.statusStrip.Text = "statusStrip1";
+            // 
+            // toolStripProgressBar1
+            // 
+            this.toolStripProgressBar1.Name = "toolStripProgressBar1";
+            this.toolStripProgressBar1.Size = new System.Drawing.Size(100, 16);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 25);
+            // 
+            // changeMainColorBtn
+            // 
+            this.changeMainColorBtn.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.changeMainColorBtn.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.changeMainColorBtn.Name = "changeMainColorBtn";
+            this.changeMainColorBtn.Size = new System.Drawing.Size(70, 22);
+            this.changeMainColorBtn.Text = "Main Color";
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(6, 25);
+            // 
+            // changeFillColorBtn
+            // 
+            this.changeFillColorBtn.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.changeFillColorBtn.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.changeFillColorBtn.Name = "changeFillColorBtn";
+            this.changeFillColorBtn.Size = new System.Drawing.Size(58, 22);
+            this.changeFillColorBtn.Text = "Fill Color";
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(624, 441);
+            this.Controls.Add(this.statusStrip);
+            this.Controls.Add(this.toolStrip);
+            this.Controls.Add(this.menuStrip);
+            this.MainMenuStrip = this.menuStrip;
             this.Name = "MainForm";
             this.Text = "Simple Paint";
+            this.menuStrip.ResumeLayout(false);
+            this.menuStrip.PerformLayout();
+            this.toolStrip.ResumeLayout(false);
+            this.toolStrip.PerformLayout();
+            this.statusStrip.ResumeLayout(false);
+            this.statusStrip.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStrip toolStrip;
+        private System.Windows.Forms.ToolStripButton pencilBtn;
+        private System.Windows.Forms.StatusStrip statusStrip;
+        private System.Windows.Forms.ToolStripProgressBar toolStripProgressBar1;
+        private System.Windows.Forms.ToolStripButton lineBtn;
+        private System.Windows.Forms.ToolStripButton rectangleBtn;
+        private System.Windows.Forms.ToolStripButton circleBtn;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripButton changeMainColorBtn;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripButton changeFillColorBtn;
     }
 }
 
